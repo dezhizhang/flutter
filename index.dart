0f98ebba6d4b56bae8d37dadd59a32f50c7673a3
dezhizhang <1541609448@qq.com>
2019-06-25 22:46:00 +0800

    变量与常量

--- index.dart
+++ index.dart
@@ -1,4 +1,32 @@
-main() {
-  print('hell run dart');
+// main() {
+//   print('hell run dart');
   
-}
+// }
+
+// void main() {
+//   print('hello run dart');
+// }
+
+// main() {
+//   print('hello run dart');
+
+// }
+
+
+// main() {
+//   var str = 'hello';
+//   print(str);
+
+// }
+
+// main() {
+//   var num = 123;
+//   print(num);
+// }
+
+// main() {
+//   const num = 123;
+//   print(num);
+// }
+
+
