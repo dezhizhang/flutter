0668819bbf0f7ffa4fc930cd9bab68a22516536e
dezhizhang <1541609448@qq.com>
2019-06-25 23:09:03 +0800

    list类型

--- index.dart
+++ index.dart
@@ -29,4 +29,22 @@
 //   print(num);
 // }
 
+// main() {
+//   var list = ['1','2','3','4'];
+//   print(list.length);
+// }
+
+// main() {
+//   var list = ['1','2','3'];
+//   print(list[1]);
+// }
+
+// main() {
+//   var list = new List();
+//   list.add(111);
+//   list.add(222);
+//   list.add(333);
+//   list.add(444);
+//   print(list);
+// }
 
