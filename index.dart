ac131a971fde1dbd15ba0cf7ad779496181b3e25
dezhizhang <1541609448@qq.com>
2019-06-27 21:11:04 +0800

    方法

--- index.dart
+++ index.dart
@@ -1,4 +1,117 @@
-main() {
-  print('123');
+// main() {
+//   print('123');
 
-}
+// }
+
+// main() {
+//    for(var i=0;i<10;i++){
+//      print(i);
+//    }
+// }
+
+// main() {
+//   for(var i=0;i<50;i++) {
+//     if(i%2==0) {
+//       print(i);
+//     }
+//   }
+// }
+
+// main() {
+//   var sum = 1;
+//   for(var i=1;i<10;i++) {
+//     sum *= i;
+//     print(sum);
+//   }
+// }
+
+// main() {
+//   var num = 1;
+//   while(num < 10) {
+//     num ++;
+//     print(num);
+
+//   }
+// }
+
+// main() {
+//   var num = 1;
+//   var sum = 0;
+//   while (num < 10) {
+//     sum += num;
+//     num++;
+//     print(sum);
+//   }
+// }
+
+// main() {
+//   for(var i=0;i<10;i++) {
+//     if(i==5) {
+//       continue;
+//     }
+//     print(i);
+
+//   }
+// }
+
+// main() {
+//   for(var i=0;i<10;i++) {
+//     if(i==5) {
+//       continue;
+//     }
+//     print(i);
+    
+//   }
+// }
+
+// main() {
+//   // for(var i=0;i<10;i++) {
+//   //   if(i==5) {
+//   //     continue;
+//   //   }
+//   //   print(i);
+
+//   // }
+
+//   var list = [];
+//   list.add(111);s
+//   list.add(222);
+//   print(list);
+
+// }
+
+
+// main() {
+//   var list = [1,2,3,4,5];
+//   var myList = list.reversed.toList();
+//   print(myList);
+  
+
+// }
+
+// main() {
+//   var list = [1,2,3,4,5];
+//   var myList = list.reversed.toList();
+//   print(myList);
+
+// }
+
+// main() {
+//   var list = [1,2,3,4,5,6,7];
+//   var myList = list.reversed.toList();
+//   print(myList);
+
+// }
+
+// main() {
+//   var list = [];
+//   list.addAll([1,2,3,4,5,6,7]);
+//   print(list);
+
+// }
+
+// main() {
+//   var list = ['5','4','2','1'];
+//   print(list.indexOf('1'));
+
+// }
