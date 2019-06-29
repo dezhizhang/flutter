8607ff811faf05cb1fc1e9f47f335a41c0df31d8
dezhizhang <1541609448@qq.com>
2019-06-29 16:05:52 +0800

    静态属性和静态方法

--- index.dart
+++ index.dart
@@ -1,7 +1,16 @@
-import 'person.dart';
+// import 'person.dart';
 
-main() {
-  var p1 = new Person('哈哈', 123);
-  p1.printInfo();
-  
-}
+// main() {
+//   var p1 = new Person('哈哈', 123);
+//   p1.printInfo();
+
+// }
+
+// class Person{
+//   static String name = '张三';
+
+// }
+
+// main() {
+//   print(Person.name);
+// }
