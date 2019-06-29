8be3863aa7d5126f5298911033c481ba50629941
dezhizhang <1541609448@qq.com>
2019-06-29 19:32:57 +0800

    继承

--- index.dart
+++ index.dart
@@ -32,20 +32,61 @@
 
 // }
 
-class Person {
-   String name = '哈哈';
-   int age = 123;
-   void printInfo() {
-     print('${this.name} ----${this.age}');
-   }
+// class Person {
+//    String name = '哈哈';
+//    int age = 123;
+//    void printInfo() {
+//      print('${this.name} ----${this.age}');
+//    }
+// }
+
+// class Web extends Person{
+
+// }
+
+// main() {
+//   var w = new Web();
+//   w.printInfo();
+
+// }
+
+// class Person{
+//   String name = '123';
+//   int age = 12;
+//   void printInfo() {
+//     print('${this.name} ---${this.age}');
+//   }
+// }
+
+// class Web extends Person{
+
+// }
+
+// main() {
+//   var w = new Web();
+//   w.printInfo();
+
+// }
+
+class Person{
+  String name = '123';
+  int age = 12;
+  Person(String name,int age) {
+    this.name = name;
+    this.age = age;
+  }
+  printInfo() {
+    print('${this.name} -----${this.age}');
+  }
 }
 
-class Web extends Person{
+
+class Web extends Person {
+  Web(String name, int age) : super(name, age);
 
 }
 
 main() {
-  var w = new Web();
+  var w = new Web('哈哈',123);
   w.printInfo();
-  
 }
