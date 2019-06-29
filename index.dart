2fe2cd44617f5727f4fd04f1e334d74390d932be
dezhizhang <1541609448@qq.com>
2019-06-29 19:53:59 +0800

    抽像类

--- index.dart
+++ index.dart
@@ -1,92 +1,18 @@
-// import 'person.dart';
+abstract class Animal{
+  eat();
 
-// main() {
-//   var p1 = new Person('哈哈', 123);
-//   p1.printInfo();
-
-// }
-
-// class Person{
-//   static String name = '张三';
-
-// }
-
-// main() {
-//   print(Person.name);
-// }
-
-// class Person{
-//   static String name = '哈哈';
-// }
-
-// main() {
-//   print(Person.name);
-// }
-
-// class Person{
-//   static String name = '哈哈';
-// }
-
-// main() {
-//   print(Person.name);
-
-// }
-
-// class Person {
-//    String name = '哈哈';
-//    int age = 123;
-//    void printInfo() {
-//      print('${this.name} ----${this.age}');
-//    }
-// }
-
-// class Web extends Person{
-
-// }
-
-// main() {
-//   var w = new Web();
-//   w.printInfo();
-
-// }
-
-// class Person{
-//   String name = '123';
-//   int age = 12;
-//   void printInfo() {
-//     print('${this.name} ---${this.age}');
-//   }
-// }
-
-// class Web extends Person{
-
-// }
-
-// main() {
-//   var w = new Web();
-//   w.printInfo();
-
-// }
-
-class Person{
-  String name = '123';
-  int age = 12;
-  Person(String name,int age) {
-    this.name = name;
-    this.age = age;
-  }
-  printInfo() {
-    print('${this.name} -----${this.age}');
-  }
 }
 
-
-class Web extends Person {
-  Web(String name, int age) : super(name, age);
-
+class Dog extends Animal{
+  @override
+  eat() {
+  
+    return '你是那个蛾';
+  }
 }
 
 main() {
-  var w = new Web('哈哈',123);
-  w.printInfo();
+  var d = new Dog();
+  print(d.eat());
+
 }
