dc6b1db8003a14e55ca7f4c5df396f2741e94d61
dezhizhang <1541609448@qq.com>
2019-06-29 19:23:07 +0800

    类的继承

--- index.dart
+++ index.dart
@@ -14,3 +14,38 @@
 // main() {
 //   print(Person.name);
 // }
+
+// class Person{
+//   static String name = '哈哈';
+// }
+
+// main() {
+//   print(Person.name);
+// }
+
+// class Person{
+//   static String name = '哈哈';
+// }
+
+// main() {
+//   print(Person.name);
+
+// }
+
+class Person {
+   String name = '哈哈';
+   int age = 123;
+   void printInfo() {
+     print('${this.name} ----${this.age}');
+   }
+}
+
+class Web extends Person{
+
+}
+
+main() {
+  var w = new Web();
+  w.printInfo();
+  
+}
