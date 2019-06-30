7320446242710dc2cd2ef792db956511a3a975df
dezhizhang <1541609448@qq.com>
2019-06-30 23:04:28 +0800

    自定义组件

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 
-// void main() => runApp(MyApp());
+void main() => runApp(MyApp());
 
-// class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Text(
+        '你好刘德华',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          fontSize: 14.0,
+          color: Colors.white
+        ),
+      )
+    );
+  }
 
-// }
-
-void main() {
-  runApp(new Center(
-    child:new Text(
-      '你好张德志',
-      textDirection:TextDirection.ltr
-    )
-  ));
 }
+
+
