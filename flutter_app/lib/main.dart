95e11588817a28d1b1342810e695845c4e8bc578
dezhizhang <1541609448@qq.com>
2019-07-01 21:45:07 +0800

    圆形图片

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -19,26 +19,41 @@ class MyApp extends StatelessWidget{
   }
 }
 
+// class HomeContent extends StatelessWidget{
+//   @override 
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     return Center(
+//       child: Container(
+//         // child: Image.network('https://www.itying.com/images/201905/thumb_img/1101_thumb_G_1557845381862.jpg'),
+//         height: 300,
+//         width: 300,
+//         decoration: BoxDecoration(
+//           color: Colors.yellow,
+//           borderRadius: BorderRadius.circular(150),
+//           image: DecorationImage(
+//             image: NetworkImage('https://www.itying.com/images/201905/thumb_img/1101_thumb_G_1557845381862.jpg'),
+//             fit: BoxFit.cover
+//           )
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 class HomeContent extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Center(
       child: Container(
-        // child: Image.network('https://www.itying.com/images/201905/thumb_img/1101_thumb_G_1557845381862.jpg'),
-        height: 300,
-        width: 300,
-        decoration: BoxDecoration(
-          color: Colors.yellow,
-          borderRadius: BorderRadius.circular(150),
-          image: DecorationImage(
-            image: NetworkImage('https://www.itying.com/images/201905/thumb_img/1101_thumb_G_1557845381862.jpg'),
-            fit: BoxFit.cover
-          )
+        child: ClipOval(
+          child: Image.network('https://www.itying.com/images/201905/thumb_img/1101_thumb_G_1557845381862.jpg'),
         ),
       ),
     );
   }
+  
 }
 
 
