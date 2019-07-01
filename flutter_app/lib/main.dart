08a9f8b909a9d7500e1f46e2e0592d85d7a987a9
dezhizhang <1541609448@qq.com>
2019-07-01 22:19:28 +0800

    图片组件

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -25,14 +25,14 @@ class MyApp extends StatelessWidget{
 //     // TODO: implement build
 //     return Center(
 //       child: Container(
-//         // child: Image.network('https://www.itying.com/images/201905/thumb_img/1101_thumb_G_1557845381862.jpg'),
+//         // child: Image.network('https://www.bdidu.com/images/201905/thumb_img/1101_thumb_G_1557845381862.jpg'),
 //         height: 300,
 //         width: 300,
 //         decoration: BoxDecoration(
 //           color: Colors.yellow,
 //           borderRadius: BorderRadius.circular(150),
 //           image: DecorationImage(
-//             image: NetworkImage('https://www.itying.com/images/201905/thumb_img/1101_thumb_G_1557845381862.jpg'),
+//             image: NetworkImage('https://www.baidu.com/images/201905/thumb_img/1101_thumb_G_1557845381862.jpg'),
 //             fit: BoxFit.cover
 //           )
 //         ),
@@ -41,19 +41,34 @@ class MyApp extends StatelessWidget{
 //   }
 // }
 
-class HomeContent extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Center(
-      child: Container(
-        child: ClipOval(
-          child: Image.network('https://www.itying.com/images/201905/thumb_img/1101_thumb_G_1557845381862.jpg'),
-        ),
-      ),
-    );
-  }
+// class HomeContent extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     return Center(
+//       child: Container(
+//         child: ClipOval(
+//           child: Image.network('https://www.itying.com/images/201905/thumb_img/1101_thumb_G_1557845381862.jpg'),
+//         ),
+//       ),
+//     );
+//   }
   
-}
+// }
+
+// class HomeContent extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     return Center(
+//       child: Container(
+//         width: 300,
+//         height: 300,
+//         child: Image.asset('images/2.jpeg'),
+//       ),
+//     );
+//   }
+  
+// }
 
 
