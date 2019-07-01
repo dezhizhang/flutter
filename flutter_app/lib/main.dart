e1039232e978e4d60a044351f9df4bee983e047c
dezhizhang <1541609448@qq.com>
2019-07-01 21:39:58 +0800

    圆形图片

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -2,24 +2,6 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     // TODO: implement build
-//     return  MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: Text('我是一个App'),
-//         ),
-//         body: HomeContent(),
-//       ),
-//       theme: ThemeData(
-//         primarySwatch: Colors.yellow 
-//       ),
-//     );
-//   }
-
-// }
 
 class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
@@ -42,26 +24,16 @@ class HomeContent extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
     return Center(
-      // child: Text(
-      //   '张德志你好',
-      //   textDirection: TextDirection.ltr,
-      // ),
       child: Container(
-        child: Text(
-          '我是一个文本',
-          textAlign: TextAlign.right,
-          style: TextStyle(
-            fontSize: 16.0,
-            color: Colors.red
-          ),
-          ),
-        height: 300.0,
-        width: 300.0,
+        // child: Image.network('https://www.itying.com/images/201905/thumb_img/1101_thumb_G_1557845381862.jpg'),
+        height: 300,
+        width: 300,
         decoration: BoxDecoration(
           color: Colors.yellow,
-          border: Border.all(
-            color: Colors.blue,
-            width: 2.0
+          borderRadius: BorderRadius.circular(150),
+          image: DecorationImage(
+            image: NetworkImage('https://www.itying.com/images/201905/thumb_img/1101_thumb_G_1557845381862.jpg'),
+            fit: BoxFit.cover
           )
         ),
       ),
