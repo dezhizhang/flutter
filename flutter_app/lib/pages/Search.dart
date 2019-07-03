98d6f77707deb13855d0e17659f6aa847d018524
dezhizhang <1541609448@qq.com>
2019-07-03 23:29:53 +0800

    顶部切换

--- flutter_app/lib/pages/Search.dart
+++ flutter_app/lib/pages/Search.dart
@@ -6,18 +6,30 @@ class SearchPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('我是搜索页'),
-        backgroundColor: Colors.green,
-        leading: IconButton(
-          icon: Icon(Icons.search),
-          onPressed: (){
-            print('1');
-          },
-        )
+    return  DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('我是搜索页'),
+          centerTitle: true,
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(text: '热门'),
+              Tab(text: '我的')
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            ListTile(
+              title: Text('我是执门'),
+            ),
+            ListTile(
+              title: Text('我的页面'),
+            )
+          ],
+        ),
       ),
-      body: Text("搜索页面区域")
     );
   }
   
