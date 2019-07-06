328fbfb01d802d7e2050cd06a67a70e34e79a938
dezhizhang <1541609448@qq.com>
2019-07-06 20:49:56 +0800

    get获取数据

--- flutter_app/lib/pages/GET.dart
+++ flutter_app/lib/pages/GET.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'dart:convert';
+
 
 class GetPage extends StatefulWidget {
   GetPage({Key key}) : super(key: key);
@@ -19,12 +21,31 @@ class _GetPageState extends State<GetPage> {
   }
   @override
   Widget build(BuildContext context) {
+  
     return Container(
        child: Scaffold(
          appBar: AppBar(
            title: Text('get请求'),
          ),
-         body: Text('我是请求数据'),
+         body: Container(
+           child: Column(
+             children: <Widget>[
+               RaisedButton(
+                  child: Text('获取数据'),
+                  color: Colors.pink,
+                  textColor: Colors.white,
+                  elevation: 10,
+                  onPressed: () async{
+                    var result = await http.get('https://cnodejs.org/api/v1/topics');
+                    if(result.statusCode == 200) {
+                       print(json.decode(result.body));
+                    }
+
+                  },
+               )
+             ],
+           ),
+         )
        ),
     );
   }
