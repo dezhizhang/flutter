fd70e9467d9dd6d2c3954dbf0b9523f7861803a8
dezhizhang <1541609448@qq.com>
2019-07-07 20:19:22 +0800

    适配

--- app/lib/pages/Home.dart
+++ app/lib/pages/Home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -7,10 +10,44 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Widget swiperWidget() {
+    List<Map> imageList = [
+      {"url": "https://www.itying.com/images/flutter/slide01.jpg"},
+      {"url": "https://www.itying.com/images/flutter/slide02.jpg"},
+      {"url": "https://www.itying.com/images/flutter/slide03.jpg"},
+    ];
+    return Container(
+      child: AspectRatio(
+        aspectRatio: 2/1,
+        child: Swiper(
+          itemBuilder: (BuildContext context,int index) {
+            return new Image.network(imageList[index]['url'],fit: BoxFit.fill);
+          },
+          itemCount: imageList.length,
+          pagination: new SwiperPagination(),
+          autoplay: true,
+        )
+      ),
+    );
+  }
+
+  Widget titleWidget(value) {
+    return Container(
+      height: ScreenUtil.getInstance().setHeight(46),
+      padding: EdgeInsets.all(20),
+      child: Text(value,style: TextStyle(
+        color: Colors.black54
+      )),
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Text('我是首页1111111111'),
+    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
+    return ListView(
+      children: <Widget>[
+        swiperWidget(),
+        titleWidget('猜你喜欢')
+      ],
     );
   }
 }
