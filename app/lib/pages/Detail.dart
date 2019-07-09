b3dd7b4a1f9e9fa86af75ab63f3ef2c11c740f55
dezhizhang <1541609448@qq.com>
2019-07-09 22:54:46 +0800

    渲染数据

--- app/lib/pages/Detail.dart
+++ app/lib/pages/Detail.dart
@@ -2,6 +2,8 @@ import 'package:app/utils/ScreenAdaper.dart';
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import '../config/Config.dart';
+import '../model/ProductModel.dart';
+
 
 
 class DetailPage extends StatefulWidget {
@@ -13,67 +15,90 @@ class DetailPage extends StatefulWidget {
 
 class _DetailPageState extends State<DetailPage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  List detailData = [];
+  int page = 1;
+ 
+  void initState() {
+    super.initState();
+    this.getDetailList();
+  }
+  getDetailList() async{
+     var result  = await Dio().get('${Config.baseURL}/api/plist?cid=${widget.arguments['cid']}&page=${this.page}');
+     var detailList = ProductModel.fromJson(result.data);
+     print(detailList.result);
+
+     setState(() {
+        this.detailData.addAll(detailList.result);
+     });
+  }
   Widget detailListWidget() {
     return  Container(
         // padding: EdgeInsets.only(l10),
         margin: EdgeInsets.only(top: ScreenAdaper.height(80)),
         child: ListView.builder(
           itemBuilder: (context,index) {
-            return Column(
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Container(
-                      width: ScreenAdaper.width(180),
-                      height: ScreenAdaper.height(180),
-                      child: Image.network('https://jd.itying.com/images/flutter/list2.jpg',fit: BoxFit.cover),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
+            if(this.detailData.length > 0) {
+              String pic = this.detailData[index].pic;
+              return Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Container(
+                        width: ScreenAdaper.width(180),
                         height: ScreenAdaper.height(180),
-                        padding: EdgeInsets.all(10),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text('只整个事哈哈哈哈俣',maxLines: 2,overflow: TextOverflow.ellipsis),
-                            Row(
-                              children: <Widget>[
-                                Container(
-                                  height: ScreenAdaper.height(36),
-                                  margin: EdgeInsets.only(right: 10),
-                                  padding: EdgeInsets.fromLTRB(10, 3, 10, 0),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    color: Color.fromRGBO(230, 230, 230, 0.9),
-                                  ),
-                                  child: Text('4G'),
-                                ),
-                                 Container(
-                                  height: ScreenAdaper.height(36),
-                                  margin: EdgeInsets.only(right: 10),
-                                  padding: EdgeInsets.fromLTRB(10,3, 10, 0),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    color: Color.fromRGBO(230, 230, 230, 0.9),
-                                  ),
-                                  child: Text('4G',),
-                                )
-                              ],
-                            ),
-                            Text('￥990',style: TextStyle(color: Colors.pink,fontSize: 14))
-                          ],
-                        )
+                        child: Image.network('${Config.baseURL}/${pic.replaceAll('\\', '/')}',fit: BoxFit.cover),
                       ),
-                    )
-                  ],
-                ),
-                Divider(height: 16)
-              ],
-            );
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          height: ScreenAdaper.height(180),
+                          padding: EdgeInsets.all(10),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text('${this.detailData[index].title}',maxLines: 2,overflow: TextOverflow.ellipsis),
+                              Row(
+                                children: <Widget>[
+                                  Container(
+                                    height: ScreenAdaper.height(36),
+                                    margin: EdgeInsets.only(right: 10),
+                                    padding: EdgeInsets.fromLTRB(10, 3, 10, 0),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Color.fromRGBO(230, 230, 230, 0.9),
+                                    ),
+                                    child: Text('评价人数'),
+                                  ),
+                                  Container(
+                                    height: ScreenAdaper.height(36),
+                                    margin: EdgeInsets.only(right: 10),
+                                    padding: EdgeInsets.fromLTRB(10,3, 10, 0),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Color.fromRGBO(230, 230, 230, 0.9),
+                                    ),
+                                    child: Text('商品分类',),
+                                  )
+                                ],
+                              ),
+                              Text('￥${this.detailData[index].price}',style: TextStyle(color: Colors.pink,fontSize: 14))
+                            ],
+                          )
+                        ),
+                      )
+                    ],
+                  ),
+                  Divider(height: 16)
+                ],
+              );
+            } else {
+              return Container(
+                child: Text('暂无数据'),
+              );
+            }
           },
-          itemCount: 10,
+          itemCount: this.detailData.length
         ),
       );
   }
