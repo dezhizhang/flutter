9e52663a34a09888c7f7696cebda1cedf3c4371a
dezhizhang <1541609448@qq.com>
2019-07-11 21:11:43 +0800

    清空历史记录

--- app/lib/pages/Search.dart
+++ app/lib/pages/Search.dart
@@ -61,9 +61,93 @@ class _SearchPageState extends State<SearchPage> {
                     borderRadius: BorderRadius.circular(10)
                   ),
                   child: Text('女装'),
+                ),
+                 Container(
+                  padding: EdgeInsets.only(left: 20,right: 20,top: 10,bottom: 10),
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(233, 233, 233, 0.9),
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: Text('男装'),
+                ),
+                 Container(
+                  padding: EdgeInsets.only(left: 20,right: 20,top: 10,bottom: 10),
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(233, 233, 233, 0.9),
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: Text('童装'),
+                ),
+                 Container(
+                  padding: EdgeInsets.only(left: 20,right: 20,top: 10,bottom: 10),
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(233, 233, 233, 0.9),
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: Text('衣服'),
                 )
               ],
+            ),
+            Divider(),
+            Container(
+              child: Text('历史记录',style: Theme.of(context).textTheme.title),
+            ),
+            Divider(),
+            Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text('鞋子'),
+                ),
+                Divider(),
+                ListTile(
+                  title: Text('鞋子'),
+                ),
+                Divider(),
+                ListTile(
+                  title: Text('鞋子'),
+                ),
+                Divider(),
+                ListTile(
+                  title: Text('鞋子'),
+                ),
+                Divider(),
+                ListTile(
+                  title: Text('鞋子'),
+                ),
+                Divider(),
+                ListTile(
+                  title: Text('鞋子'),
+                ),
+                Divider()
+              ],
+            ),
+            SizedBox(height: 100),
+            InkWell(
+              onTap: (){
+
+              },
+              child:Container(
+              width: ScreenAdaper.width(500),
+              height: ScreenAdaper.height(64),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Color.fromRGBO(233, 233, 233, 1),
+                  width: 1
+                )
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.delete),
+                  Text('清空历史记录')
+                ],
+              ),
             )
+            )
+            
           ],
         ),
       )
