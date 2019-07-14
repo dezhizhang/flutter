dbfcd2116bec837d998b7384ef266b67cc4b35b1
dezhizhang <1541609448@qq.com>
2019-07-14 19:57:59 +0800

    拆分路由

--- xiaozhi/lib/main.dart
+++ xiaozhi/lib/main.dart
@@ -1,36 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import './Search.dart';
+import './routes.dart';
 
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final routes = {
-    '/search':(context,{arguments}) => SearchPage(arguments:arguments)
-  };
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Tabs(),
-      onGenerateRoute: (RouteSettings settings) {
-        final String name = settings.name;
-        final Function pageContentBuilder = this.routes[name];
-        if(pageContentBuilder !=null) {
-          if(settings.arguments !=null) {
-            final Route route = MaterialPageRoute(
-              builder: (context) => pageContentBuilder(context,arguments:settings.arguments)
-
-            );
-            return route;
-          }else {
-            final Route route = MaterialPageRoute(
-              builder: (context) => pageContentBuilder(context)
-            );
-            return route;
-          }
-        } 
-      },
+      onGenerateRoute: onGenerateRoute,
       theme: ThemeData(
         primaryColor: Colors.pink
       ),
---
+++ xiaozhi/lib/routes.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import './Search.dart';
+
+final routes = {
+  '/search':(context,{arguments}) => SearchPage(arguments: arguments)
+};
+
+var onGenerateRoute = (RouteSettings settings) {
+  final String name = settings.name;
+  final Function pageContentBuilder = routes[name];
+  if(pageContentBuilder !=null) {
+    if(settings.arguments !=null) {
+      final Route route = MaterialPageRoute(
+        builder: (context) => pageContentBuilder(context,arguments:settings.arguments)
+      );
+      return route;
+    } else {
+      final Route route = MaterialPageRoute(
+        builder: (context) => pageContentBuilder(context)
+      );
+      return route;
+    }
+  }
+};
