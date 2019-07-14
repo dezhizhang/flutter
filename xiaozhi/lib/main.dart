07fba1de6bd99dde9ad777cef6a65e29b9fb9b89
dezhizhang <1541609448@qq.com>
2019-07-14 13:20:14 +0800

    网格布局

--- xiaozhi/lib/main.dart
+++ xiaozhi/lib/main.dart
@@ -21,23 +21,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// class HomeContent extends StatelessWidget {
-//   const HomeContent({Key key}) : super(key: key);
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListView.builder(
-//       itemCount: listData.length,
-//       itemBuilder: (context,index) {
-//         return ListTile(
-//           leading: Image.network('${listData[index]['imageUrl']}'),
-//           title: Text('${listData[index]['title']}'),
-//           subtitle: Text('${listData[index]['author']}'),
-//         );
-//       },
-//     );
-//   }
-// }
 
 class HomeContent extends StatelessWidget {
   const HomeContent({Key key}) : super(key: key);
@@ -46,42 +30,25 @@ class HomeContent extends StatelessWidget {
   Widget build(BuildContext context) {
     return GridView.count(
       crossAxisCount: 2,
-      crossAxisSpacing: 20,
-      mainAxisSpacing: 20,
-      padding: EdgeInsets.all(10),
-      childAspectRatio: 0.7,
-
-      children: <Widget>[
-        Container(
-          width: 300,
-          height: 300,
-          color: Colors.pink,
-          child: Text('第一个容量'),
-        ),
-        Container(
-          width: 300,
-          height: 300,
-          color: Colors.yellow,
-          child: Text('第二个容器'),
-        ),
-        Container(
-          width: 300,
-          height: 300,
-          color: Colors.pink,
-          child: Text('第一个容量'),
-        ),
-        Container(
-          width: 300,
-          height: 300,
-          color: Colors.yellow,
-          child: Text('第二个容器'),
-        )
-      ],
+      children:listData.map((value) {
+        return Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.pink,
+              width: 2
+            )
+          ),
+          child: Column(
+            children: <Widget>[
+              Image.network('${value['imageUrl']}'),
+              Text('${value['title']}')
+            ],
+          ),
+        );
+      }).toList(),
     );
   }
 }
-
-
 
 
    
