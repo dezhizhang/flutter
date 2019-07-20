3b4735870084a1e4f91f64c99de4c84930c7a778
dezhizhang <1541609448@qq.com>
2019-07-20 12:09:08 +0800

    文本输入框

--- xiaozhi/lib/Text.dart
+++ xiaozhi/lib/Text.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class TextPage extends StatefulWidget {
   TextPage({Key key}) : super(key: key);
@@ -15,43 +16,42 @@ class _TextPageState extends State<TextPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('表单页面'),
-      ),
-      body: Container(
-       
-        child: Column(
-          children: <Widget>[
-            TextField(
-              controller: userName,
-              onChanged: (value) {
-                setState(() {
-                 this.userName.text = value;
-                });
-              },
-              decoration: InputDecoration(
-                icon: Icon(Icons.home),
-                labelText: '密码',
-                hintText: '请输入内容',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.only(left: 10,right: 10),
-              child: RaisedButton(
-                child: Text('登录'),
-                color: Colors.pink,
-                textColor: Colors.white,
-                onPressed: () {
-                  print(this.userName.text);
-                },
-              )
-            )
-          ],
+        appBar: AppBar(
+          title: Text('表单页面'),
         ),
-
-      ),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: userName,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.home),
+                  labelStyle: TextStyle(color: Colors.yellow),
+                  hintText: '请输入',
+                  border: OutlineInputBorder()
+                ),
+                onChanged: (value) {
+                  setState(() {
+                   this.userName.text = value; 
+                  });
+                },
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.only(left: 10,right: 10),
+                child: RaisedButton(
+                  child: Text('登录'),
+                  color: Colors.pink,
+                  textColor: Colors.white,
+                  onPressed: () {
+                    print(this.userName.text);
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
     );
   }
 }
+
