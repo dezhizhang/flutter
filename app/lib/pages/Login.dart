2e3c3b1284131b009606db16d47febbdeac1f18f
dezhizhang <1541609448@qq.com>
2019-08-01 21:04:12 +0800

    登录页面

--- app/lib/pages/Login.dart
+++ app/lib/pages/Login.dart
@@ -39,25 +39,82 @@ class _LoginPageState extends State<LoginPage> {
           ),
           SizedBox(height: 20),
           Container(
+            padding: EdgeInsets.only(left: 10,right: 10),
             width: double.infinity,
             height: ScreenAdaper.height(68),
             decoration: BoxDecoration(
               border: Border(
                 bottom: BorderSide(
                   width: 1,
-                  color: Colors.black12
+                  color: Colors.pink
                 )
               )
             ),
             child: TextField(
               decoration: InputDecoration(
-                hintText: '用户名',
+                hintText: '请输入用户名',
               ),
               onChanged: (value) {
 
               },
             ),
+          ),
+          SizedBox(height: 10),
+          Container(
+            padding: EdgeInsets.only(left: 10,right: 10),
+            width: double.infinity,
+            height: ScreenAdaper.height(68),
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  width: 1,
+                  color: Colors.pink
+                )
+              )
+            ),
+            child: TextField(
+              obscureText:true,
+              decoration: InputDecoration(
+                hintText: '请输入密码',
+                
+              ),
+              onChanged: (value) {
+
+              },
+            ),
+          ),
+          SizedBox(height: 10),
+          Container(
+            padding: EdgeInsets.only(left: 10,right: 10),
+            child: Stack(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('忘记密码'),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Text('新用户注册'),
+
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 128),
+          Container(
+            height: ScreenAdaper.height(68),
+            padding: EdgeInsets.only(left: 10,right: 10),
+            child: RaisedButton(
+              
+              child: Text('登录'),
+              color: Colors.pink,
+              textColor: Colors.white,
+              onPressed: () {
+
+              },
+            ),
           )
+
         ],
       )
     );
