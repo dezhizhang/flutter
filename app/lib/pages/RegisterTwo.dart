4111771b1d1801da923c39fef24fdc5cf109f2bf
dezhizhang <1541609448@qq.com>
2019-08-01 22:09:17 +0800

    第二步完成

--- app/lib/pages/RegisterTwo.dart
+++ app/lib/pages/RegisterTwo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../utils/ScreenAdaper.dart';
 
 class RegisterTwo extends StatefulWidget {
   RegisterTwo({Key key}) : super(key: key);
@@ -13,7 +14,65 @@ class _RegisterTwoState extends State<RegisterTwo> {
       appBar: AppBar(
         title: Text('注册第二步'),
       ),
-      body: Text('新用户注册'),
+      body: Container(
+        padding: EdgeInsets.only(left: 10,right: 10,top: 30),
+        child: ListView(
+          children: <Widget>[
+            Container(
+              child: Text('请输入15083356190手机收到的验证码'),
+            ),
+            SizedBox(height: 10),
+            Stack(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        width: 1,
+                        color: Colors.pink
+                      )
+                    )
+                  ),
+                  child: TextField(
+                  decoration: InputDecoration(
+                    hintText: '请输入验证码'
+                  ),
+                  onChanged: (value) {
+
+                  },
+                  ),
+                ),
+                Positioned(
+                  right: 0,
+                  top: 0,
+                  child: RaisedButton(
+                    color: Colors.pink,
+                    textColor: Colors.white,
+                    child: Text('发送验证码'),
+                    onPressed: () {
+
+                    },
+                  ),
+                )
+                
+              ],
+            ),
+            SizedBox(height: 100),
+            Container(
+              width: double.infinity,
+              height: ScreenAdaper.height(68),
+              child: RaisedButton(
+                child: Text('下一步'),
+                color: Colors.pink,
+                textColor: Colors.white,
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/registerthree');
+                },
+              ),
+            )
+          ],
+        ),
+      )
     );
   }
 }
