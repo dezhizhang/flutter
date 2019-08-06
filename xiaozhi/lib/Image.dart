e653af68bdbcba5243bda3b3406de10830c4c7f3
dezhizhang <1541609448@qq.com>
2019-08-06 21:40:49 +0800

    图片组件

--- xiaozhi/lib/Image.dart
+++ xiaozhi/lib/Image.dart
@@ -24,18 +24,32 @@ class _ImagePageState extends State<ImagePage> {
         //     alignment: Alignment.center
         //   ),
         // ),
+        // child: Container(
+        //   width: 300,
+        //   height: 300,
+        //   decoration: BoxDecoration(
+        //     color: Colors.pink,
+        //     borderRadius: BorderRadius.circular(150),
+        //     border: Border.all(width: 1,color: Colors.pink),
+        //     image: DecorationImage(
+        //       image: NetworkImage('http://pic.baike.soso.com/p/20130828/20130828161137-1346445960.jpg'),R
+        //       fit: BoxFit.cover
+        //     )
+        //   ),
+        // ),
         child: Container(
-          width: 300,
-          height: 300,
-          decoration: BoxDecoration(
-            color: Colors.pink,
-            borderRadius: BorderRadius.circular(150),
-            border: Border.all(width: 1,color: Colors.pink),
-            image: DecorationImage(
-              image: NetworkImage('http://pic.baike.soso.com/p/20130828/20130828161137-1346445960.jpg'),
-              fit: BoxFit.cover
-            )
-          ),
+         
+          child: ClipOval(
+            
+            child: Image.network(
+              'http://pic.baike.soso.com/p/20130828/20130828161137-1346445960.jpg',
+              width: 300,
+              height: 300,
+              fit: BoxFit.cover,
+              
+              
+            ),
+          )
         ),
       )
     );
