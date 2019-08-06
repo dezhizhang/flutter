5d9a4dced0f8c58d2a2ccbf7490dd91d90d3119a
dezhizhang <1541609448@qq.com>
2019-08-06 21:58:36 +0800

    第垂直列表

--- xiaozhi/lib/ListView.dart
+++ xiaozhi/lib/ListView.dart
@@ -9,7 +9,16 @@ class ListViewPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('列表组件'),
       ),
-      body: Text('列表组件'),
+      body: ListView(
+        children: <Widget>[
+            ListTile(
+              leading: Image.network('http://pic.baike.soso.com/p/20130828/20130828161137-1346445960.jpg',width: 100, height: 100),
+              title: Text('列表组件'),
+              subtitle: Text('详情信息'),
+
+            )
+        ],
+      )
     );
   }
 }
