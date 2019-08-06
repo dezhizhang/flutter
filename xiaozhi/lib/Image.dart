2492c66be51b56f1f82377a3c1d79323b984f829
dezhizhang <1541609448@qq.com>
2019-08-06 21:32:30 +0800

    圆形图片

--- xiaozhi/lib/Image.dart
+++ xiaozhi/lib/Image.dart
@@ -14,14 +14,27 @@ class _ImagePageState extends State<ImagePage> {
         title: Text('图片组件'),
       ),
       body:Center(
+        // child: Container(
+        //   width: 1200,
+        //   height: 300,
+        //   child: Image.network(
+        //     'http://pic.baike.soso.com/p/20130828/20130828161137-1346445960.jpg',
+        //     fit: BoxFit.cover,
+        //     repeat: ImageRepeat.repeatX,
+        //     alignment: Alignment.center
+        //   ),
+        // ),
         child: Container(
-          width: 1200,
+          width: 300,
           height: 300,
-          child: Image.network(
-            'http://pic.baike.soso.com/p/20130828/20130828161137-1346445960.jpg',
-            fit: BoxFit.cover,
-            repeat: ImageRepeat.repeatX,
-            alignment: Alignment.center
+          decoration: BoxDecoration(
+            color: Colors.pink,
+            borderRadius: BorderRadius.circular(150),
+            border: Border.all(width: 1,color: Colors.pink),
+            image: DecorationImage(
+              image: NetworkImage('http://pic.baike.soso.com/p/20130828/20130828161137-1346445960.jpg'),
+              fit: BoxFit.cover
+            )
           ),
         ),
       )
