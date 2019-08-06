124d89a79db4a94816adb39524f6d0880649ec56
dezhizhang <1541609448@qq.com>
2019-08-06 21:19:09 +0800

    图片组件

--- xiaozhi/lib/Image.dart
+++ xiaozhi/lib/Image.dart
@@ -13,7 +13,18 @@ class _ImagePageState extends State<ImagePage> {
       appBar: AppBar(
         title: Text('图片组件'),
       ),
-      body: Text('图片组件'),
+      body:Center(
+        child: Container(
+          width: 1200,
+          height: 300,
+          child: Image.network(
+            'http://pic.baike.soso.com/p/20130828/20130828161137-1346445960.jpg',
+            fit: BoxFit.cover,
+            repeat: ImageRepeat.repeatX,
+            alignment: Alignment.center
+          ),
+        ),
+      )
     );
   }
 }
