01b37cb5cac55c4f0109604fe1c5d0a40b346177
dezhizhang <1541609448@qq.com>
2019-08-06 22:51:45 +0800

    动态数据

--- xiaozhi/lib/ListView.dart
+++ xiaozhi/lib/ListView.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './data.dart';
 
 class ListViewPage extends StatelessWidget {
   const ListViewPage({Key key}) : super(key: key);
@@ -9,45 +10,25 @@ class ListViewPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('列表组件'),
       ),
-      // body: ListView(
-      //   children: <Widget>[
-      //       ListTile(
-      //         leading: Image.network('http://pic.baike.soso.com/p/20130828/20130828161137-1346445960.jpg',width: 100, height: 100),
-      //         title: Text('列表组件'),
-      //         subtitle: Text('详情信息'),
-
-      //       )
-      //   ],
-      // )
-      body: Container(
-        height: 180,
-        child: ListView(
-          scrollDirection: Axis.horizontal,
-          children: <Widget>[
-            Container(
-              width: 180,
-              height: 180,
-              color: Colors.yellow,
+      
+      body: ListView.builder(
+        itemCount: listData.length,
+        itemBuilder: (context,index) {
+          return Container(
+            // child: Image.network('${listData[index]['imageUrl']}')
+            child: Column(
+              children: <Widget>[
+                Container(
+                  child: Image.network('${listData[index]['imageUrl']}'),
+                ),
+                Container(
+                  child: Text('${listData[index]['title']}'),
+                )
+              ],
             ),
-            Container(
-              width: 180,
-              height: 180,
-              color: Colors.pink,
-            ),
-            Container(
-              width: 180,
-              height: 180,
-              color: Colors.green,
-            ),
-            Container(
-              width: 180,
-              height: 180,
-              color: Colors.orange,
-            )
-
-          ],
-        ),
-      ),
+          );
+        },
+      )
     );
   }
 }
