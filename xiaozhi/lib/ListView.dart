0de75b624c465ff181cf7e136f00c8cd1792c702
dezhizhang <1541609448@qq.com>
2019-08-06 22:22:22 +0800

    水平列表

--- xiaozhi/lib/ListView.dart
+++ xiaozhi/lib/ListView.dart
@@ -9,16 +9,45 @@ class ListViewPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('列表组件'),
       ),
-      body: ListView(
-        children: <Widget>[
-            ListTile(
-              leading: Image.network('http://pic.baike.soso.com/p/20130828/20130828161137-1346445960.jpg',width: 100, height: 100),
-              title: Text('列表组件'),
-              subtitle: Text('详情信息'),
+      // body: ListView(
+      //   children: <Widget>[
+      //       ListTile(
+      //         leading: Image.network('http://pic.baike.soso.com/p/20130828/20130828161137-1346445960.jpg',width: 100, height: 100),
+      //         title: Text('列表组件'),
+      //         subtitle: Text('详情信息'),
 
+      //       )
+      //   ],
+      // )
+      body: Container(
+        height: 180,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            Container(
+              width: 180,
+              height: 180,
+              color: Colors.yellow,
+            ),
+            Container(
+              width: 180,
+              height: 180,
+              color: Colors.pink,
+            ),
+            Container(
+              width: 180,
+              height: 180,
+              color: Colors.green,
+            ),
+            Container(
+              width: 180,
+              height: 180,
+              color: Colors.orange,
             )
-        ],
-      )
+
+          ],
+        ),
+      ),
     );
   }
 }
