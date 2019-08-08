4aa6f918c0c0338932aa76a005c5db1eb246a155
dezhizhang <1541609448@qq.com>
2019-08-08 20:17:32 +0800

    层叠绷 件

--- xiaozhi/lib/Strack.dart
+++ xiaozhi/lib/Strack.dart
@@ -9,16 +9,26 @@ class StrackPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('层叠组件'),
       ),
-      body: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Container(
-            width: 400,
-            height: 400,
-            color: Colors.pink,
-          ),
-          Text('我是一个文本',style: TextStyle(color: Colors.white))
-        ],
+      body: Container(
+        width: 400,
+        height: 400,
+        color: Colors.pink,
+        child: Stack(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Icon(Icons.home,color: Colors.white),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Icon(Icons.search,color: Colors.yellow),
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Icon(Icons.send,color: Colors.white),
+            )
+          ],
+        ),
       )
     );
   }
