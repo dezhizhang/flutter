2865ebe476b842b361357a265089656614846bec
dezhizhang <1541609448@qq.com>
2019-08-08 20:37:51 +0800

    对齐与定位

--- xiaozhi/lib/Strack.dart
+++ xiaozhi/lib/Strack.dart
@@ -2,56 +2,19 @@ import 'package:flutter/material.dart';
 
 class StrackPage extends StatelessWidget {
   const StrackPage({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('层叠组件'),
       ),
-      // body: Container(
-      //   width: 400,
-      //   height: 400,
-      //   color: Colors.pink,
-      //   child: Stack(
-      //     children: <Widget>[
-      //       Align(
-      //         alignment: Alignment.centerLeft,
-      //         child: Icon(Icons.home,color: Colors.white),
-      //       ),
-      //       Align(
-      //         alignment: Alignment.center,
-      //         child: Icon(Icons.search,color: Colors.yellow),
-      //       ),
-      //       Align(
-      //         alignment: Alignment.centerRight,
-      //         child: Icon(Icons.send,color: Colors.white),
-      //       )
-      //     ],
-      //   ),
-      // )
       body: Container(
-        width: 400,
-        height: 400,
-        color: Colors.pink,
-        child: Stack(
-          children: <Widget>[
-            Positioned(
-              left: 10,
-              top: 100,
-              child: Icon(Icons.home,color: Colors.white),
-            ),
-            Positioned(
-              left: 100,
-              top: 100,
-              child: Icon(Icons.search,color: Colors.white),
-            ),
-            Positioned(
-              left: 200,
-              top: 100,
-              child: Icon(Icons.security,color: Colors.white),
-            )
-          ],
+        width: 120,
+        height: 120,
+        color: Colors.blue[50],
+        child: Align(
+          alignment: Alignment.topRight,
+          child: FlutterLogo(size: 60),
         ),
       ),
     );
