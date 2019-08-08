5bea680a0068058bf68d5fdb60bf3f8875d95650
dezhizhang <1541609448@qq.com>
2019-08-08 22:18:55 +0800

    特珠情况处理

--- xiaozhi/lib/Colum.dart
+++ xiaozhi/lib/Colum.dart
@@ -9,42 +9,15 @@ class ColumPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('colum组件'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text('你好中国'),
-              Text('你好哈哈')
-            ],
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text('你好中国'),
-              Text('你好世界')
-            ],
-          ),
-          Row(
-            mainAxisAlignment:MainAxisAlignment.end,
-            textDirection: TextDirection.rtl,
-            children: <Widget>[
-              Text('你好中国'),
-              Text('你好世界')
-            ],
-          ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            verticalDirection: VerticalDirection.up,
-            children: <Widget>[
-              Text('你好',style: TextStyle(fontSize: 20,color: Colors.pink)),
-              Text('你好世界')
-            ],
-          )
-
-        ],
+      body: ConstrainedBox(
+        constraints: BoxConstraints(minWidth: double.infinity),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text('你好'),
+            Text('中国')
+          ],
+        ),
       ),
     );
   }
