e0a92d29693f1a94faa5db2b33956713767c59c7
dezhizhang <1541609448@qq.com>
2019-08-09 22:07:31 +0800

    修改属性

--- app/lib/provder/Counter.dart
+++ app/lib/provder/Counter.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class Counter with ChangeNotifier{
    int _count = 0;
    int get count => _count;
-   
+
    increment() {
      this._count ++;
      notifyListeners(); //更新状态
