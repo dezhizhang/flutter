a941698083194eb2a8c4bdfc0711daaaa057a5ec
dezhizhang <1541609448@qq.com>
2019-08-09 18:04:00 +0800

    事件机制

--- xiaozhi/lib/Event.dart
+++ xiaozhi/lib/Event.dart
@@ -1,5 +1,51 @@
 import 'package:flutter/material.dart';
 
+// class EventPage extends StatefulWidget {
+//   EventPage({Key key}) : super(key: key);
+
+//   _EventPageState createState() => _EventPageState();
+// }
+
+// class _EventPageState extends State<EventPage> {
+//    double _top = 0.0;
+//    double _left = 0.0;
+
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('事件'),
+//       ),
+//       body: Stack(
+//         children: <Widget>[
+//           Positioned(
+//             top: this._top,
+//             left: this._left,
+//             child: GestureDetector(
+//               child: CircleAvatar(child: Text('A')),
+//               onPanDown: (DragDownDetails e) {
+//                 print('用户手指按下：${e.globalPosition}');
+//               },
+//               onPanUpdate: (DragUpdateDetails e) {
+//               //用户手指滑动时，更新偏移，重新构建
+//               setState(() {
+//                 _left += e.delta.dx;
+//                 _top += e.delta.dy;
+//               });
+//              },
+//              onPanEnd: (DragEndDetails  e) {
+//                print(e.velocity);
+//              },
+//             ),
+//           )
+//         ],
+//       ),
+     
+//     );
+//   }
+// }
+
 class EventPage extends StatefulWidget {
   EventPage({Key key}) : super(key: key);
 
@@ -7,41 +53,13 @@ class EventPage extends StatefulWidget {
 }
 
 class _EventPageState extends State<EventPage> {
-   double _top = 0.0;
-   double _left = 0.0;
-
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return  Scaffold(
       appBar: AppBar(
         title: Text('事件'),
       ),
-      body: Stack(
-        children: <Widget>[
-          Positioned(
-            top: this._top,
-            left: this._left,
-            child: GestureDetector(
-              child: CircleAvatar(child: Text('A')),
-              onPanDown: (DragDownDetails e) {
-                print('用户手指按下：${e.globalPosition}');
-              },
-              onPanUpdate: (DragUpdateDetails e) {
-              //用户手指滑动时，更新偏移，重新构建
-              setState(() {
-                _left += e.delta.dx;
-                _top += e.delta.dy;
-              });
-             },
-             onPanEnd: (DragEndDetails  e) {
-               print(e.velocity);
-             },
-            ),
-          )
-        ],
-      ),
-     
+      body: Text('事件'),
     );
   }
 }
