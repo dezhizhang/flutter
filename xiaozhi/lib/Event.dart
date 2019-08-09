ba0e4fecb0de13d5573a7bc9232bb92903e15e14
dezhizhang <1541609448@qq.com>
2019-08-09 17:38:30 +0800

    移动事件

--- xiaozhi/lib/Event.dart
+++ xiaozhi/lib/Event.dart
@@ -7,12 +7,9 @@ class EventPage extends StatefulWidget {
 }
 
 class _EventPageState extends State<EventPage> {
-  String _operation = '';
-  void updateText(String text) {
-    setState(() {
-      this._operation = text; 
-    });
-  }
+   double _top = 0.0;
+   double _left = 0.0;
+
 
   @override
   Widget build(BuildContext context) {
@@ -20,20 +17,31 @@ class _EventPageState extends State<EventPage> {
       appBar: AppBar(
         title: Text('事件'),
       ),
-      body: Center(
-        child: GestureDetector(
-          child: Container(
-            alignment: Alignment.center,
-            color: Colors.pink,
-            width: 200,
-            height: 200,
-            child: Text('${this._operation}',style: TextStyle(color: Colors.white)),
-          ),
-          onTap: () => updateText('Tap'),
-          onDoubleTap: () => updateText('DoubleTap'),
-          onLongPress: () => updateText('LongPress'),
-        ),
-      )
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            top: this._top,
+            left: this._left,
+            child: GestureDetector(
+              child: CircleAvatar(child: Text('A')),
+              onPanDown: (DragDownDetails e) {
+                print('用户手指按下：${e.globalPosition}');
+              },
+              onPanUpdate: (DragUpdateDetails e) {
+              //用户手指滑动时，更新偏移，重新构建
+              setState(() {
+                _left += e.delta.dx;
+                _top += e.delta.dy;
+              });
+             },
+             onPanEnd: (DragEndDetails  e) {
+               print(e.velocity);
+             },
+            ),
+          )
+        ],
+      ),
+     
     );
   }
 }
