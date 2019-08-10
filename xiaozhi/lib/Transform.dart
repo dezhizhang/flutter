98080d650c99842fbc8b55ce44536a7b024978d1
dezhizhang <1541609448@qq.com>
2019-08-10 14:17:06 +0800

    状态管理

--- xiaozhi/lib/Transform.dart
+++ xiaozhi/lib/Transform.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import './provider/Counter.dart';
+import './provider/Card.dart';
 
 class TransformPage extends StatefulWidget {
   TransformPage({Key key}) : super(key: key);
@@ -12,13 +13,20 @@ class _TransformPageState extends State<TransformPage> {
   @override
   Widget build(BuildContext context) {
     var count = Provider.of<Counter>(context);
+    var cart = Provider.of<Cart>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('transform'),
       ),
       body: Container(
-        height: 400,
-        child:Text('${count.count}') ,
+        child:RaisedButton(
+          color: Colors.pink,
+          textColor: Colors.white,
+          child: Text('增加数据'),
+          onPressed: () {
+            cart.addList('你好呀');
+          },
+        )
       )
     );
   }
