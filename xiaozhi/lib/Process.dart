743904285e600720ba30dfb9751e2faf79a2a510
dezhizhang <1541609448@qq.com>
2019-08-12 16:31:24 +0800

    自定义进度条

--- xiaozhi/lib/Process.dart
+++ xiaozhi/lib/Process.dart
@@ -8,24 +8,41 @@ class ProcessPage extends StatefulWidget {
   _ProcessPageState createState() => _ProcessPageState();
 }
 
-class _ProcessPageState extends State<ProcessPage> {
+class _ProcessPageState extends State<ProcessPage> with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _animationController = new AnimationController(vsync: this,duration: Duration(seconds: 3));
+    _animationController.forward();
+    _animationController.addListener(() => setState(() =>{}));
+  }
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('process'),
       ),
-      // body: LinearProgressIndicator(
-      //   backgroundColor: Colors.yellow,
-      //   valueColor: AlwaysStoppedAnimation(Colors.white),
-      // )
-      body: CircularProgressIndicator(
-        value: 0.5,
-        backgroundColor: Colors.yellow,
-        valueColor: AlwaysStoppedAnimation(Colors.pink),
-        strokeWidth: 4,
-      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: LinearProgressIndicator(
+                backgroundColor: Colors.grey[200],
+                valueColor: ColorTween(begin: Colors.grey,end: Colors.blue).animate(_animationController),
+                value: _animationController.value,
+              ),
+            )
+          ],
+        ),
+      )
     );
-
   }
 }
