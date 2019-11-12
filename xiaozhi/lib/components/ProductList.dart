98bdb26fbf0830696ab58d010bc25b0bfa8c0e13
dezhizhang <1541609448@qq.com>
2019-11-12 23:39:20 +0800

    修改字体

--- xiaozhi/lib/components/ProductList.dart
+++ xiaozhi/lib/components/ProductList.dart
@@ -36,7 +36,14 @@ class ListItem extends StatelessWidget{
             child: Image.network('https://www.itying.com/images/flutter/7.png',fit:BoxFit.cover),
           ),
           SizedBox(height: 6),
-          Text('2019潮流韩版蝙蝠衫',textAlign: TextAlign.left),
+          Text(
+            '2019潮流韩版蝙蝠衫',maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              color: Color.fromRGBO(51,51,51,1),
+              fontSize: 12
+            ),
+          ),
           SizedBox(height: 6),
           Text('￥100')
         ],
