03e2948bbde9a2193791b50727388b7ea61e649c
dezhizhang <1541609448@qq.com>
2019-12-01 20:06:01 +0800

    map类型

--- dart/day03/main.dart
+++ dart/day03/main.dart
@@ -1,7 +1,66 @@
+// main() {
+//   String str1 = "hello";
+//   String str2 = "dart";
+//   String str = "${str1} ${str2}";
+//   print(str);
+  
+// }
+
+// main() {
+//   List list = ["red","yellow","pink"];
+//   print(list);
+// }
+
+// main() {
+//   List list = ["red","yellow","pink"];
+//   print(list);
+  
+// }
+
+// main() {
+//   List list = new List();
+//   list.add("张三");
+//   list.add("李四");
+//   list.add("王五");
+//   print(list);
+// }
+
+// main() {
+//     List list = new List<String>();
+//     list.add("张三");
+//     list.add("王五");
+//     list.add("马六");
+//     print(list);
+
+// }
+
+// main() {
+//   var person = {
+//     "name":"张三",
+//     "age":20
+//   };
+
+//   print(person["name"]);
+
+// }
+
+// main() {
+//   // var person = {
+//   //   "name":"张三",
+//   //   "age":44
+//   // };
+//   // print(person["name"]);
+
+//   var person = new Map();
+//   person["name"] = "张三";
+//   person["age"] = 44;
+//   print(person);
+// }
+
 main() {
-  String str1 = "hello";
-  String str2 = "dart";
-  String str = "${str1} ${str2}";
-  print(str);
+  Map person = new Map();
+  person["name"] = "张三";
+  person["age"] = 44;
+  print(person);
   
 }
