c3937b8262c5c8d790b78041f318bd4ba79f207e
dezhizhang <1541609448@qq.com>
2019-12-12 22:55:43 +0800

    可选参数

--- dart/day05/main.dart
+++ dart/day05/main.dart
@@ -52,15 +52,35 @@
 //   print(count(10));
 // }
 
-int count(int n) {
-  var sum = 0;
-  for (var i=0;i < n;i++) {
-    sum += i;
+// int count(int n) {
+//   var sum = 0;
+//   for (var i=0;i < n;i++) {
+//     sum += i;
+//   }
+//   return sum;
+// }
+
+// main() {
+//   print(count(10));
+
+// }
+
+// String printUserInfo(String name, int age)  {
+//   return "你的名字是:${name}你的打年龄是:${age}";
+// }
+
+
+// main() {
+//   print(printUserInfo("周华建",44));
+// }
+
+String printUserInfo(String name,[int age]) {
+  if(age!=null) {
+    return "你的姓名是:${name},你的年龄是:${age}";
   }
-  return sum;
+  return "你的姓名是:${name},年龄:保密";
 }
 
 main() {
-  print(count(10));
-
+  print(printUserInfo("周大大"));
 }
