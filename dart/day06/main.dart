17d9c718d5e806e11b5828e859e3d003b572fa0c
dezhizhang <1541609448@qq.com>
2019-12-12 23:39:57 +0800

    map函数

--- dart/day06/main.dart
+++ dart/day06/main.dart
@@ -5,10 +5,32 @@
 //   });
 // }
 
+// main() {
+//   List list = [1,3,5,7,9];
+//   List newList = list.map((value){
+//     if(value >2) {
+//       return value * 2;
+//     }
+//     return value;
+//   }).toList();
+//   print(newList);
+// }
+
+// main() {
+//   List list = [1,2,3,4,5,6];
+//   List newList = list.map((value){
+//     if(value > 2) {
+//       return value * 2;
+//     }
+//     return value;
+//   }).toList();
+//   print(newList);
+// }
+
 main() {
-  List list = [1,3,5,7,9];
+  List list = [1,2,3,4,5,6,7,8];
   List newList = list.map((value){
-    if(value >2) {
+    if(value > 2) {
       return value * 2;
     }
     return value;
