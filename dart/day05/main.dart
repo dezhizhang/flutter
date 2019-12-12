eeae42147e76c33a4fb24c27f9d01d6dbff63995
dezhizhang <1541609448@qq.com>
2019-12-12 22:43:22 +0800

    调用方法

--- dart/day05/main.dart
+++ dart/day05/main.dart
@@ -1,7 +1,53 @@
 
-String printInfo() {
-  return "周华建你好";
+// String printInfo() {
+//   return "周华建你好";
+// }
+// main() {
+//   print(printInfo());
+// }
+
+// String printInfo() {
+//   return "周大大你好";
+// }
+
+// main() {
+//   print(printInfo());
+// }
+
+// String printInfo() {
+//   return "你好周大大";
+// }
+
+// main() {
+//   print(printInfo());
+// }
+
+// List printInfo() {
+//   return ["111111","2222222","33333"];
+// }
+
+// main() {
+//   print(printInfo());
+
+// }
+
+// List printInfo() {
+//   return ["11111","222222","333333"];
+// }
+
+// main() {
+//   print(printInfo());
+
+// }
+
+int count(int n) {
+  var sum = 0;
+  for (var i=0;i < n;i++) {
+    sum += i;
+  }
+  return sum;
 }
+
 main() {
-  print(printInfo());
+  print(count(10));
 }
