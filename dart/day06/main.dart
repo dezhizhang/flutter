c2011db926e1804a866487bbdc5522179f197d7e
dezhizhang <1541609448@qq.com>
2019-12-12 23:32:12 +0800

    列表函数

--- dart/day06/main.dart
+++ dart/day06/main.dart
@@ -1,8 +1,17 @@
+// main() {
+//   List list = ["red","yellow","pink","green"];
+//   list.forEach((value) => {
+//     print(value)
+//   });
+// }
+
 main() {
-  List list = ["red","yellow","pink","green"];
-  list.forEach((value) => {
-    print(value)
-  });
-
-
+  List list = [1,3,5,7,9];
+  List newList = list.map((value){
+    if(value >2) {
+      return value * 2;
+    }
+    return value;
+  }).toList();
+  print(newList);
 }
