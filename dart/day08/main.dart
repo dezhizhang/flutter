f4c1759bec683fd02df12890577444655f5aec06
dezhizhang <1541609448@qq.com>
2019-12-16 21:40:18 +0800

    抽像方法

---
+++ dart/day08/main.dart
@@ -0,0 +1,17 @@
+
+abstract class Animal{
+  eat();
+}
+
+class Dog extends Animal{
+  @override
+  eat() {
+    // TODO: implement eat
+    print("小狗在吃东西");
+  }
+}
+
+main() {
+  Dog d = new Dog();
+  d.eat();
+}
